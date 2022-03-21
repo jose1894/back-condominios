cf655868b93800849dfbc81006874ff636f4276c
Jose Guerra <jose1894@gmail.com>
2022-03-21 05:25:49 -0500

    first commit

---
+++ middlewares/index.js
@@ -0,0 +1,12 @@
+
+const validarCampos  = require('../middlewares/validar-campos')
+const validarJWT     = require('../middlewares/validar-jwt')
+const validaRoles    = require('../middlewares/validar-roles')
+const { validarArchivoSubir } = require('./validar-archivo')
+
+module.exports = {
+    ...validarCampos,
+    ...validarJWT,
+    ...validaRoles,
+    ...validarArchivoSubir
+}
---
+++ models/usuario.js
@@ -0,0 +1,49 @@
+
+const { Schema, model } = require('mongoose')
+
+
+const UserSchema = Schema({
+    fullName: {
+        type: String,
+        required: [ true, 'El nombre es obligatorio']
+    },
+    username: {
+        type: String,
+        required: [ true, 'El username es obligatorio']
+    },
+    email: {
+        type: String,
+        required: [ true, 'El email es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [ true, 'El correo es obligatorio']
+    },
+    avatar: {
+        type: String,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE', 'VENTAS_ROLE']
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+    },
+    google: {
+        type: Boolean,
+        default:false
+    },
+})
+
+UserSchema.methods.toJSON = function() {
+    const { __v, password, _id, ...usuario } = this.toObject()
+
+    usuario.id = _id
+
+    return usuario
+}
+
+module.exports = model( 'Usuario', UserSchema )
